cfea39d97c5d38534bc24f3bf60f852931e6365a
Itai Manyati <itai@databalk.africa>
2021-11-02 05:05:38 +0200

    Initial commit

---
+++ ProductManager/Data/ProductsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductManager.Models;
+
+namespace ProductManager.Data
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
